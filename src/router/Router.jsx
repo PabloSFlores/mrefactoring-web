0127baa9200781b270347f9c0b8ad06815e7e738
Cristina <20213tn151@utez.edu.mx>
2024-01-25 01:17:09 -0600

    [UPDATE]Actualizacion de rutas

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter as Router,Route, Routes } from 'react-router-dom'
 import MisionVison from '../components/MisionVison'
 import Navbar from '../components/Navbar'
+import CalculoMetrica from '../modules/CalculoMetricas/view/CalculoMetrica'
+import CalculosObtenidos from '../modules/CalculosObtenidos/view/CalculosObtenidos'
+import Refactorizacion from '../modules/RefactorizacionCodigo/view/Refactorizacion'
 const AppRouter = () => {
   return (
     <Router>
@@ -11,6 +14,9 @@ const AppRouter = () => {
             <Navbar/>
             <Routes>
             <Route path='/misionVision' element={<MisionVison/>} />
+            <Route path='/calculoMetrica' element={<CalculoMetrica/>} />
+            <Route path='/calculoObtenido' element={<CalculosObtenidos/>} />
+            <Route path='/refactorizacion' element={<Refactorizacion/>} />
             </Routes>
             </>
         } />
