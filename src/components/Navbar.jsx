39dc955eb71adccb2e8b5ec98a75adea0a56cfc0
Cristina <20213tn151@utez.edu.mx>
2024-01-25 23:36:43 -0600

    [UPDATE] navbar creado

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,23 @@
-import { Menubar } from "primereact/menubar";
-import logos from "../assets/img/Group 2.png";
-import cenidet from "../assets/img/cenidet.png";
-import educacion from "../assets/img/LOGO_SEP.png";
-import Tec from "../assets/img/TecNM_logo.png";
-import gobierno from "../assets/img/LOGO_GobiernoMexico.png";
 import React from "react";
+import logo from "../assets/img/LOGO_GobiernoMexico.png";
+import foto1 from "../assets/img/cenidet.png";
+import foto2 from "../assets/img/LOGO_SEP.png";
+import foto3 from "../assets/img/TecNM_logo.png";
 
 const Navbar = () => {
-  const start = (
-    <>
-      <img alt="logo" src={gobierno} height="40" className="mr-3" />
-      <img alt="logo" src={educacion} height="40" className="mr-3" />
-      <img alt="logo" src={Tec} height="50" className="mr-5" />
-    </>
-  );
-
-  const end = <img alt="logo" src={cenidet} height="40" className="mr-2" />;
-
-  const items = [
-    {
-      label:  <h2 className="navbar-label">MRefactoring Web</h2>
-    },
-  ];
-
   return (
-    <>
-    <Menubar model={items} start={start} end={end} className="sticky-navbar" />
-    <br/>
-    </>
+    <div className="navbar-mio">
+      <div className="navbar-logo">
+        <img src={logo} alt="Logo" className="image-navbar mx-4" />
+        <img src={foto2} alt="Logo" className="image-navbar mx-2" />
+        <img src={foto3} alt="Logo" className="image-navbar mx-2" />
+        <div className="logo-text" style={{ marginLeft: "20%" }}>MRefactoring</div>
+      </div>
+      
+      <div className="navbar-images">
+        <img src={foto1} className="image-navbar-end" alt="Foto 1" />
+      </div>
+    </div>
   );
 };
 
